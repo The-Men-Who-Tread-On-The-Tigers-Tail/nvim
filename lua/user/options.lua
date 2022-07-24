9aa8b05775d28545b648a25cd2b20c086fc8af96
The-Men-Who-Tread-On-The-Tigers-Tail <The-Men-Who-Tread-On-The-Tigers-Tail@posteo.de>
2022-07-24 15:25:26 +0200

    feat: set options to my taste
    relativenumber off, wrap lines on,
    and guifont agave
    
    master

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -29,13 +29,13 @@ local options = {
   laststatus = 3,
   showcmd = false,
   ruler = false,
-  relativenumber = true,                   -- set relative numbered lines
+  relativenumber = false,                   -- set relative numbered lines
   numberwidth = 4,                         -- set number column width to 2 {default 4}
   signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
-  wrap = false,                            -- display lines as one long line
+  wrap = true,                            -- display lines as one long line
   scrolloff = 8,                           -- is one of my fav
   sidescrolloff = 8,
-  guifont = "monospace:h17",               -- the font used in graphical neovim applications
+  guifont = "agave Nerd Font Mono:h16",               -- the font used in graphical neovim applications
   -- colorcolumn = "80",
   -- colorcolumn = "120",
 
