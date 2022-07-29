de9e7a2f3fe072f41ec0237197c22d7c450a8c84
The-Men-Who-Tread-On-The-Tigers-Tail <The-Men-Who-Tread-On-The-Tigers-Tail@posteo.de>
2022-07-29 21:22:56 +0200

    feat: change easing_function to quadratic
    development

--- lua/user/neoscroll.lua
+++ lua/user/neoscroll.lua
@@ -11,7 +11,7 @@ neoscroll.setup {
   -- use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
   -- respect_scrolloff = true, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
   -- cursor_scrolls_alone = false, -- The cursor will keep on scrolling even if the window cannot scroll further
-  -- easing_function = nil, -- Default easing function
+  easing_function = "quadratic", -- Default easing function, possbile options: quadratic, cubic, quartic, quintic, circular, sine
   -- pre_hook = nil, -- Function to run before the scrolling animation starts
   -- post_hook = nil, -- Function to run after the scrolling animation ends
 }
